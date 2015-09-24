cf31c5a49c3259d376cd4090a9ccfedbb66f6d30
adhirajC <kingadhi@hotmail.com>
2015-09-24 12:43:24 -0400

    sdfsdf

---
+++ src/lab13110/BuddyInfo.java
@@ -0,0 +1,54 @@
+package lab13110;
+
+
+public class BuddyInfo {
+
+String name;
+
+String address;
+
+String phone_nummber;
+
+	public String getName() {
+
+	return name;
+
+}
+	
+	public BuddyInfo(String name, String address, String phone_nummber){
+		super();
+	}
+public void setName(String name) {
+
+	this.name = name;
+
+}
+
+public String getAddress() {
+
+	return address;
+
+}
+
+public void setAddress(String address) {
+
+	this.address = address;
+
+}
+
+public String getPhone_nummber() {
+
+	return phone_nummber;
+
+}
+
+public void setPhone_nummber(String phone_nummber) {
+
+	this.phone_nummber = phone_nummber;
+
+}
+
+
+
+
+}
